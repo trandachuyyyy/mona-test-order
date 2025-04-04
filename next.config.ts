d7979239683a40cb6aa214dfab8fb3c981cd6dff
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2025-04-04 19:40:35 +0700

    updated

--- next.config.ts
+++ next.config.ts
@@ -15,8 +15,8 @@ const nextConfig: NextConfig = {
                 hostname: "*",
             },
         ],
-        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840], // Kích thước thiết bị hỗ trợ
-        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384], // Kích thước hình ảnh
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     },
     async redirects() {
         return [
